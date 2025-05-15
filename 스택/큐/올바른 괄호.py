f337a1847f012f369dc6b5bd9968a2eb9359e6af
Chae-Yu <chaeyu2004@naver.com>
2025-05-15 20:55:43 +0900

    code: 올바른 괄호
    - from collections import deque
    - deque를 이용해서 pop(0)이 아니라 popleft() 사용

--- 스택/큐/올바른 괄호.py
+++ 스택/큐/올바른 괄호.py
@@ -1,18 +1,20 @@
+from collections import deque
+
 def solution(s):
     answer = True
-    temp = []
-    list_s = list(s)
-    while len(list_s)>0:
+    temp = deque()
+    list_s = deque(s)
+    while len(list_s) > 0:
         if list_s[0] == ')':
             if '(' in temp:
-                list_s.pop(0)
-                temp.pop(0)
+                list_s.popleft()
+                temp.popleft()
             else:
                 return False
         else:
             # '(' 인 경우 
             # '(' 다음에 바로 ')'인 경우 | '(' 다음에 또 '(' 인 경우
-            temp.append(list_s.pop(0))
+            temp.append(list_s.popleft())
     if len(temp) > 0 :
         return False
     return True
