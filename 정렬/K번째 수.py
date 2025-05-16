10663acf8cd1d1e7c26b6ed887152abecd3ac0f2
Chae-Yu <chaeyu2004@naver.com>
2025-05-16 21:32:47 +0900

    code: K번째 수 다른 방법 추가

--- 정렬/K번째 수.py
+++ 정렬/K번째 수.py
@@ -8,5 +8,9 @@ def solution(array, commands):
         temp.sort()        
         
         answer.append(temp[k-1])
+        
+        # answer.append(list(sorted(array[i-1:j]))[k-1])
     
     return answer
+
+    # return list(map(lambda x: sorted(array[x[0]-1:x[1]])[x[2]-1], commands))
