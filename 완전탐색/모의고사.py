960fce015b28046c36066f63d9ae30be54716377
Chae-Yu <chaeyu2004@naver.com>
2025-05-19 17:57:32 +0900

    code: 모의고사.py

---
+++ 완전탐색/모의고사.py
@@ -0,0 +1,22 @@
+def solution(answers):
+    result = []
+    p1 = [1,2,3,4,5]
+    p2 = [2,1,2,3,2,4,2,5]
+    p3 = [3,3,1,1,2,2,4,4,5,5]
+    
+    score = [0,0,0]
+    
+    for i, answer in enumerate(answers):
+        if answer == p1[i%len(p1)]:
+            score[0] +=1
+        if answer == p2[i%len(p2)]:
+            score[1] +=1
+        if answer == p3[i%len(p3)]:
+            score[2] +=1
+            
+    max_score = (max(score))
+    for i, s in enumerate(score):
+        if s == max_score:
+            result.append(i+1)
+        
+    return result
