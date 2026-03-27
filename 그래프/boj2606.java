a5c6175a22844a8338128e392386a5d5ec39fc62
Chae-Yu <chaeyu2004@naver.com>
2026-03-27 16:31:40 +0900

    code: boj2606

--- 그래프/boj2606.java
+++ 그래프/boj2606.java
@@ -1,54 +1,46 @@
 package 그래프;
-
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.io.*;
 
 public class boj2606 {
-
-    static ArrayList<Integer>[] graph;
-    static boolean[] dfsVisited;
-    static int count = 0;
+    static boolean[] visited;
+    static int[][] graph;
+    static int N;
+    static int count;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader((new InputStreamReader(System.in)));
+        // dfs
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-        int computerNum = Integer.parseInt(br.readLine());
-        int connectedComp = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
+        int T = Integer.parseInt(br.readLine());
 
-        graph = new ArrayList[computerNum + 1];
-        for (int i = 1; i <= computerNum; i++) {
-            graph[i] = new ArrayList<>();
+        graph = new int[N + 1][N + 1];
+        visited = new boolean[N + 1];
+
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            graph[a][b] = graph[b][a] = 1;
         }
 
-        for (int i = 1; i <= connectedComp; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-
-            graph[u].add(v);
-            graph[v].add(u);
-        }
-
-        for (int i = 1; i <= computerNum; i++) {
-            Collections.sort(graph[i]);
-        }
-
-        dfsVisited = new boolean[computerNum + 1];
+        count = 0;
         dfs(1);
+        System.out.println(count);
 
-        System.out.println(count - 1);
     }
 
-    static void dfs(int computer) {
-        dfsVisited[computer] = true;
-        count += 1;
-
-        for (int next : graph[computer]) {
-            if (!dfsVisited[next]) {
-                dfs(next);
+    static void dfs(int start) {
+        visited[start] = true;
+        for (int i = 1; i < N + 1; i++) {
+            if (visited[i] == false && graph[start][i] == 1) {
+                dfs(i);
+                count += 1;
             }
         }
+
     }
 }
