c271f8e5c3dd95b22606ec92c35991d81f86c4e5
Chae-Yu <chaeyu2004@naver.com>
2026-03-28 23:13:17 +0900

    code: boj1012

--- 그래프/boj1012.java
+++ 그래프/boj1012.java
@@ -1,98 +1,70 @@
 package 그래프;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class boj1012 {
 
-    static int map[][];
-    static boolean[][] visited;
+    static int[] dx = { 1, 0, -1, 0 };
+    static int[] dy = { 0, -1, 0, 1 };
+    static int[][] graph;
 
-    static int dirX[] = { 0, 0, -1, 1 };
-    static int dirY[] = { -1, 1, 0, 0 };
-    static int M, N, K;
-    static int count;
-    static int nowX, nowY;
+    static int M;
+    static int N;
+    static int K;
+
+    static boolean[][] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
-
         int T = Integer.parseInt(br.readLine());
 
+        StringTokenizer st;
         for (int i = 0; i < T; i++) {
+            int count = 0;
             st = new StringTokenizer(br.readLine());
+            M = Integer.parseInt(st.nextToken()); // 가로 길이
+            N = Integer.parseInt(st.nextToken()); // 세로 길이
+            K = Integer.parseInt(st.nextToken()); // 배추가 심어져 있는 위치의 개수
 
-            M = Integer.parseInt(st.nextToken());
-            N = Integer.parseInt(st.nextToken());
-            K = Integer.parseInt(st.nextToken());
-
-            map = new int[N][M];
+            graph = new int[N][M];
             visited = new boolean[N][M];
 
             for (int j = 0; j < K; j++) {
+                // 입력 받은 위치가 배추가 1
                 st = new StringTokenizer(br.readLine());
-
                 int x = Integer.parseInt(st.nextToken());
                 int y = Integer.parseInt(st.nextToken());
-                map[y][x] = 1;
+
+                graph[y][x] = 1;
             }
 
-            count = 0;
             for (int j = 0; j < N; j++) {
                 for (int k = 0; k < M; k++) {
-                    if (map[j][k] == 1 && !visited[j][k]) {
-                        count++;
-                        // dfs(k, j);
-                        bfs(k, j);
+                    if (graph[j][k] == 1 && visited[j][k] == false) {
+                        dfs(j, k);
+                        count += 1;
                     }
                 }
             }
-            sb.append(count).append('\n');
+
+            System.out.println(count);
+
         }
-        System.out.println(sb);
+
     }
 
-    static void dfs(int x, int y) {
+    static void dfs(int y, int x) {
         visited[y][x] = true;
 
         for (int i = 0; i < 4; i++) {
-            nowX = x + dirX[i];
-            nowY = y + dirY[i];
+            int nx = x + dx[i];
+            int ny = y + dy[i];
 
-            if (nowX >= 0 && nowX < M && nowY >= 0 && nowY < N
-                    && !visited[nowY][nowX]
-                    && map[nowY][nowX] == 1) {
-                dfs(nowX, nowY);
-            }
-        }
-    }
-
-    static void bfs(int x, int y) {
-        // 큐 안에 int 배열 들어감
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[] { x, y });
-        visited[y][x] = true;
-
-        while (!queue.isEmpty()) {
-            int[] now = queue.poll();
-            nowX = now[0];
-            nowY = now[1];
-
-            for (int i = 0; i < 4; i++) {
-                int nextX = nowX + dirX[i];
-                int nextY = nowY + dirY[i];
-
-                if (nextX >= 0 && nextX < M && nextY >= 0 && nextY < N && !visited[nextY][nextX]
-                        && map[nextY][nextX] == 1) {
-                    visited[nextY][nextX] = true;
-                    queue.add(new int[] { nextX, nextY });
+            if (nx >= 0 && ny < N && ny >= 0 && nx < M) {
+                if (graph[ny][nx] == 1 && visited[ny][nx] == false) {
+                    visited[ny][nx] = true;
+                    dfs(ny, nx);
                 }
             }
         }
