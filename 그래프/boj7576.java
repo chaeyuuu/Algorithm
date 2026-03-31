dde40ad63012ce2dfda3b92d5ba64204f01c1d34
Chae-Yu <chaeyu2004@naver.com>
2026-03-31 12:37:15 +0900

    code: boj7576

--- 그래프/boj7576.java
+++ 그래프/boj7576.java
@@ -5,11 +5,13 @@ import java.util.*;
 
 public class boj7576 {
 
-    static int box[][];
-    static boolean visited[][];
+    static int[][] tomatos;
+    static boolean[][] visited;
+
     static int[] dx = { 1, 0, -1, 0 };
     static int[] dy = { 0, -1, 0, 1 };
-    static Queue<int[]> tomatos = new LinkedList<>();;
+
+    static Queue<int[]> starts;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,55 +20,57 @@ public class boj7576 {
         int M = Integer.parseInt(st.nextToken()); // 가로
         int N = Integer.parseInt(st.nextToken()); // 세로
 
-        // 1 : 익은 토마토
-        // 0 : 익지 않은 토마토
-        // -1 :토마토 안 들어있는 곳
-
-        box = new int[N][M];
+        tomatos = new int[N][M];
         visited = new boolean[N][M];
+        starts = new LinkedList<>();
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
-                box[i][j] = Integer.parseInt(st.nextToken());
+                tomatos[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                // 1인 곳을 전부 queue에 넣기
-                if (box[i][j] == 1 && visited[i][j] == false) {
-                    tomatos.add(new int[] { i, j });
+                if (!visited[i][j] && tomatos[i][j] == 1) {
+                    // 방문하지 않았고 1이면 큐에 등록
+                    starts.add(new int[] { i, j });
                     visited[i][j] = true;
                 }
             }
         }
 
-        bfs(tomatos, N, M);
+        // bfs 실행
+        bfs(starts, N, M);
 
-        int maxDays = 0;
+        // 돌고 만약에 아직 0이 남아있으면 -1
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                if (box[i][j] == 0) {
-                    // bfs 다 돌고 0인 곳이 남아있으면 다 익지 못한 것
+                if (tomatos[i][j] == 0) {
                     System.out.println(-1);
                     return;
                 }
-
-                maxDays = Math.max(maxDays, box[i][j]);
             }
         }
 
-        // 처음 시작한 곳 빼기
-        System.out.println(maxDays - 1);
+        // 아니라면 최소 날짜 출력
+        int maxDay = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                maxDay = Math.max(maxDay, tomatos[i][j]);
+            }
+        }
 
+        System.out.println(maxDay - 1);
     }
 
-    static void bfs(Queue<int[]> tomatos, int N, int M) {
-        // 1인 곳에서 시작
-        // x가 가로, y가 세로
-        while (!tomatos.isEmpty()) {
-            int[] now = tomatos.poll();
+    static void bfs(Queue<int[]> starts, int N, int M) {
+        // 모두 익을 때까지의 최소 날짜니까 칸에다가 +1
+
+        // 이미 큐에 등록되어져왔으니까 바로
+        while (!starts.isEmpty()) {
+            int[] now = starts.poll();
             int curX = now[1];
             int curY = now[0];
 
@@ -74,15 +78,18 @@ public class boj7576 {
                 int nextX = curX + dx[i];
                 int nextY = curY + dy[i];
 
-                if (nextX >= 0 && nextX < M && nextY >= 0 && nextY < N) {
-                    if (box[nextY][nextX] == 0 && visited[nextY][nextX] == false) {
-                        // 0이고 방문 안 했으면 1로 바꾸고 visited true로 바꾸기
+                if (nextX < M && nextY < N && nextX >= 0 && nextY >= 0) {
+                    // 방문 안 했고 0이면
+                    if (!visited[nextY][nextX] && tomatos[nextY][nextX] == 0) {
+                        tomatos[nextY][nextX] = tomatos[curY][curX] + 1;
                         visited[nextY][nextX] = true;
-                        box[nextY][nextX] = box[curY][curX] + 1;
-                        tomatos.add(new int[] { nextY, nextX });
+                        starts.add(new int[] { nextY, nextX });
                     }
                 }
             }
+
         }
+
     }
+
 }
