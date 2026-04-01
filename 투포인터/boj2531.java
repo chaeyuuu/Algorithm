e9cf16ac5d91ed41ba0b4976a3a01b30b7dafe03
Chae-Yu <chaeyu2004@naver.com>
2026-04-01 12:06:21 +0900

    code: boj2351

--- 투포인터/boj2531.java
+++ 투포인터/boj2531.java
@@ -1,57 +1,65 @@
 package 투포인터;
 
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class boj2531 {
-    public static void main(String[] agrs) throws IOException {
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int plates = Integer.parseInt(st.nextToken()); // N (접시의 수)
-        int category = Integer.parseInt(st.nextToken()); // d (초밥의 가짓 수)
-        int eat = Integer.parseInt(st.nextToken()); // k (연속해서 먹는 접시의 수)
-        int coupon = Integer.parseInt(st.nextToken()); // c (쿠폰 번호 수)
+        int plates = Integer.parseInt(st.nextToken()); // N: 접시의 수
+        int sushiType = Integer.parseInt(st.nextToken()); // d: 초밥의 가짓수
+        int eatPlates = Integer.parseInt(st.nextToken()); // k: 연속해서 먹는 접시 수
+        int coupon = Integer.parseInt(st.nextToken()); // c: 쿠폰 번호
 
-        int[] arr = new int[plates];
+        int[] sushi = new int[plates];
+
         for (int i = 0; i < plates; i++) {
-            // 한 줄마다 초밥의 종류 입력받기
-            arr[i] = Integer.parseInt(br.readLine());
+            // 초밥 종류 sushi 배열에 저장
+            sushi[i] = Integer.parseInt(br.readLine());
+            // [7,9,7,30,2,7,9,25]
         }
 
-        int[] sushiCount = new int[category + 1]; // 초밥 종류별로 저장하는 카테고리
+        int[] count = new int[sushiType + 1];
         int currentType = 0;
-        for (int i = 0; i < eat; i++) {
-            if (sushiCount[arr[i]] == 0)
+
+        for (int i = 0; i < eatPlates; i++) {
+            if (count[sushi[i]] == 0)
                 // 처음 먹는 종류면 카운트 증가
                 currentType++;
-            sushiCount[arr[i]]++; // 자체 저장되어있는 값도 증가시켜주기
+            count[sushi[i]]++; // 자체 저장되어있는 값도 증가시켜주기
         }
 
-        // 현재 기준 정답을 종류의 수 (쿠폰 더하기 전으로)
         int ans = currentType;
 
-        for (int i = 0; i < arr.length; i++) {
+        // 처음 k개 다하면 이제 슬라이딩 윈도우
+        for (int i = 0; i < sushi.length; i++) {
             int total = currentType;
-            // 쿠폰 없으면 하나 추가
-            if (sushiCount[coupon] == 0)
-                total++;
+            // 카운트 센 값 중에 쿠폰 번호에 해당하는 값이
+            // 0아면 쿠폰 번호가 포함되어있지 않다는 뜻
+            if (count[coupon] == 0) {
+                total += 1;
+            }
 
-            // 저장되어있던 값이랑 total 중에 큰거
-            ans = Math.max(ans, total);
+            ans = Math.max(total, ans);
 
-            // 슬라이딩 윈도우 고고
-            sushiCount[arr[i]]--; // 접시 하나 빼기
-            if (sushiCount[arr[i]] == 0)
-                currentType--; // 더 이상 이 종류가 없으면 카운트 감소
+            // 이제 슬라이딩 윈도우
+            count[sushi[i]]--;
+            if (count[sushi[i]] == 0) {
+                currentType--;
+            }
 
-            // 다음 인덱스할건데 회전초밥이라 % 연산
-            int nextIndex = (i + eat) % plates;
-            if (sushiCount[arr[nextIndex]] == 0)
+            int nextIndex = (i + eatPlates) % plates;
+            if (count[sushi[nextIndex]] == 0)
                 currentType++;
-            sushiCount[arr[nextIndex]]++;
+            count[sushi[nextIndex]]++;
+
         }
 
         System.out.println(ans);
+
     }
+
 }
