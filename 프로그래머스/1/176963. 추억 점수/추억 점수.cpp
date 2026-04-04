563fdb14f0d6dd087a5cab886897f2cff3580497
chaeyu <130548845+chaeyuuu@users.noreply.github.com>
2026-04-05 01:47:54 +0900

    [level 1] Title: 추억 점수, Time: 1.43 ms, Memory: 4.24 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<string> name, vector<int> yearning, vector<vector<string>> photo) {
+    
+    vector<int> answer(photo.size());
+    
+    for(int i=0; i<photo.size();i++){
+        int count = 0;
+        for(int j=0; j<photo[i].size();j++){
+            for(int k=0; k<name.size();k++){
+                if(photo[i][j] == name[k]){
+                    answer[i] += yearning[k];
+                    break;
+                }
+            }
+        }
+    }
+    
+    return answer;
+}
