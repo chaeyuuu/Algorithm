f78bbd2c31d978a12f00e56ff09caed59649d520
chaeyu <130548845+chaeyuuu@users.noreply.github.com>
2026-04-28 19:54:07 +0900

    [level 1] Title: K번째수, Time: 0.42 ms, Memory: 87.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -3,15 +3,17 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-        // 0부터 1(1-1)까지 자르고 정렬한다음에 2이 위치에 있는거 픽
-        for(int i=0; i<commands.length; i++){
-            ArrayList<Integer> arr = new ArrayList<>();
-            for(int j=commands[i][0]-1; j<commands[i][1]; j++){
-                arr.add(array[j]);
-            }
-            
-            Collections.sort(arr);
-            answer[i] = arr.get(commands[i][2]-1);
+
+        for(int i = 0; i < commands.length; i++) {
+            int start = commands[i][0];
+            int end = commands[i][1];
+            int k = commands[i][2];
+
+            int[] temp = Arrays.copyOfRange(array, start - 1, end);
+    
+             Arrays.sort(temp);
+    
+            answer[i] = temp[k - 1];
         }
         
         return answer;
