f0fae9f159b25cb8c6e9f39135146f85901ddd6a
chaeyu <130548845+chaeyuuu@users.noreply.github.com>
2026-04-28 09:22:26 +0900

    [level 1] Title: 덧칠하기, Time: 1.73 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -1,22 +1,22 @@
+import java.util.*;
+import java.io.*;
+
 class Solution {
     public int solution(int n, int m, int[] section) {
         int answer = 0;
+        
+        // 칠해진 범위의 끝
         int lastPoint = 0;
         
-        // boolean[] paint = new boolean[n+1];
-        int start = section[0];
         int len = section.length;
         for(int i=0; i<len; i++){
            
-            // 아직 안 칠해진 부분이라면
-            if(section[i]>lastPoint){
-                 answer +=1;
-                
-                lastPoint = section[i]+m-1;
+            // 현재 위치가 칠해진 범위가 아니라면
+            if(section[i] > lastPoint){
+                answer++;
+                lastPoint = section[i] + m - 1;
             }
-
-           
-        
+            
         }
         
         
