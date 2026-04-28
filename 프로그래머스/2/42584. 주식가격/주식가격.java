3d250ccac7896ec07e8acb08c395cd290c7b02d8
chaeyu <130548845+chaeyuuu@users.noreply.github.com>
2026-04-28 13:53:51 +0900

    [level 2] Title: 주식가격, Time: 47.34 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,18 +1,30 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] prices) {
-        int[] answer = new int[prices.length];
         
-        for(int i = 0; i < prices.length; i++) {
-            for(int j = i + 1; j < prices.length; j++) {
+        // 스택에 초를 저장
+        Stack<Integer> stack = new Stack<>();
+        int[] results = new int[prices.length];
+        
+        stack.push(0); // 초기값 넣기
+        
+        for(int i=1; i<prices.length; i++){
+            // 인덱스를 저장..?!
+            // 다음에 들어오려는게 지금 맨 상단에 있는거보다 작으면 
+            while(!stack.isEmpty() && prices[stack.peek()] > prices[i]){
+                results[stack.peek()] = i - stack.peek();
+                stack.pop(); // 상단에 있는거 없애기
                 
-                answer[i]++; 
-                
-                if(prices[i] > prices[j]) {
-                    break;
-                }
             }
+            stack.push(i);
         }
         
-        return answer;
+        while(!stack.isEmpty()){
+            results[stack.peek()] = prices.length - stack.peek()-1;
+            stack.pop();
+        }
+        
+        return results;
     }
 }
