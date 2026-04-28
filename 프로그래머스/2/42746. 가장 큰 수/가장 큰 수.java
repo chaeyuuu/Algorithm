ce240abea90323cfae22bbbb184c922be8a6c7c4
chaeyu <130548845+chaeyuuu@users.noreply.github.com>
2026-04-28 20:05:47 +0900

    [level 2] Title: 가장 큰 수, Time: 172.89 ms, Memory: 143 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int[] numbers) {
+        String answer = "";
+        String[] strNums = new String[numbers.length];
+        for(int i=0; i<numbers.length; i++){
+            strNums[i] = String.valueOf(numbers[i]);
+        }
+        
+        Arrays.sort(strNums, (s1, s2) -> {
+            return (s2+s1).compareTo(s1+s2);
+        });
+        
+        // 숫자가 다 0이면
+        if(strNums[0].equals("0")) return "0";
+        
+        StringBuilder sb = new StringBuilder();
+        for(String s: strNums){
+            sb.append(s);
+        }
+        
+        return sb.toString();
+        
+    }
+}
