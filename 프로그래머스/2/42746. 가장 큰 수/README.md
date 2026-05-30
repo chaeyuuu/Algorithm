5e546375f3a0cefab977f49034cc1f06edd6214d
chaeyu <130548845+chaeyuuu@users.noreply.github.com>
2026-05-30 15:40:04 +0900

    [level 2] Title: 가장 큰 수, Time: 108.79 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/README.md
+++ 프로그래머스/2/42746. 가장 큰 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 수 - 42746 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746) 
 
 ### 성능 요약
 
-메모리: 143 MB, 시간: 172.89 ms
+메모리: 105 MB, 시간: 108.79 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 28일 20:05:41
+2026년 05월 30일 15:40:00
 
 ### 문제 설명
 
--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,26 +1,32 @@
 import java.util.*;
+import java.io.*;
 
 class Solution {
     public String solution(int[] numbers) {
         String answer = "";
+        
+        // 가장 큰 수를 문자열로 바꿔서 출력하면댐
+        // 문자열로 바꾸면 자동으로 정렬? 됨
+        
         String[] strNums = new String[numbers.length];
+        
+        
         for(int i=0; i<numbers.length; i++){
             strNums[i] = String.valueOf(numbers[i]);
         }
         
-        Arrays.sort(strNums, (s1, s2) -> {
+        Arrays.sort(strNums, (s1, s2)->{
+            // 내림 차순 정렬
             return (s2+s1).compareTo(s1+s2);
         });
         
-        // 숫자가 다 0이면
         if(strNums[0].equals("0")) return "0";
         
         StringBuilder sb = new StringBuilder();
-        for(String s: strNums){
+        for(String s:strNums){
             sb.append(s);
         }
         
         return sb.toString();
-        
     }
 }
